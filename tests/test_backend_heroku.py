629d5df9f02c8848500b85ea6262ccff3e609f13
Tarjei Husøy <git@thusoy.com>
2016-12-06 09:39:29 -0800

    Convert indentation to spaces

--- tests/test_backend_heroku.py
+++ tests/test_backend_heroku.py
@@ -5,15 +5,15 @@ import responses
 
 @responses.activate
 def test_heroku_backend():
-	responses.add(responses.GET, 'https://api.heroku.com/apps/myapp/config-vars',
-		body='{"DATABASE_URL": "postgres://mydbhost"}', content_type='application/json')
+    responses.add(responses.GET, 'https://api.heroku.com/apps/myapp/config-vars',
+        body='{"DATABASE_URL": "postgres://mydbhost"}', content_type='application/json')
 
-	config = {
-		'heroku_api_token': 'foobar',
-		'heroku_app': 'myapp',
-		'config_key': 'DATABASE_URL',
-	}
-	backend = Heroku(**config)
-	value = backend.get()
-	assert len(responses.calls) == 1
-	assert value == 'postgres://mydbhost'
+    config = {
+        'heroku_api_token': 'foobar',
+        'heroku_app': 'myapp',
+        'config_key': 'DATABASE_URL',
+    }
+    backend = Heroku(**config)
+    value = backend.get()
+    assert len(responses.calls) == 1
+    assert value == 'postgres://mydbhost'
