89790657dcb1342e6bd24d050ea0427aeb9ffca2
Tarjei Husøy <git@thusoy.com>
2016-12-06 09:29:06 -0800

    Add project structure helpers

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import sys
+
+install_requires = [
+    'requests',
+    'toml',
+]
+
+setup(
+    name='cachish',
+    version='0.1.0',
+    author='Tarjei Husøy',
+    author_email='git@thusoy.com',
+    url='https://github.com/thusoy/cachish',
+    description="Stale cache-ish thingie",
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Web Environment',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        # 'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+)
