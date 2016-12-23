2db4fd4d1970408062387d6dade21528267b76ab
Tarjei Husøy <git@thusoy.com>
2016-12-23 09:58:40 +0100

    Set correct classifiers

--- setup.py
+++ setup.py
@@ -30,10 +30,10 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         # 'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        # 'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
     ],
 )
