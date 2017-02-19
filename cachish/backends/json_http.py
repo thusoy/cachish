288bc0045f6ad2101f8946f773da26dde571f9a2
Tarjei Husøy <git@thusoy.com>
2017-02-19 09:39:26 -0800

    Enable not filtering response from JsonHttp

--- cachish/backends/json_http.py
+++ cachish/backends/json_http.py
@@ -7,15 +7,19 @@ class JsonHttp(object):
             field=None,
             ):
         assert url is not None
-        assert field is not None
         self.session = Session()
         self.session.trust_env = False # ignore .netrc auth
         self.target_url = url
-        self.fields = [field] if isinstance(field, str) else field
+        if field:
+            self.fields = [field] if isinstance(field, str) else field
+        else:
+            self.fields = None
 
 
     def get(self):
         response = self.session.get(self.target_url, timeout=5)
         response.raise_for_status()
         response_json = response.json()
+        if not self.fields:
+            return response_json
         return {key: response_json[key] for key in self.fields}
--- tests/test_json_http_backend.py
+++ tests/test_json_http_backend.py
@@ -5,13 +5,34 @@ import responses
 
 @responses.activate
 def test_heroku_backend():
-    response = {"key": "value"}
+    response = {
+        "key1": "value1",
+        "key2": "value2",
+    }
     responses.add(responses.GET, 'https://example.com',
         json=response)
 
     config = {
         'url': 'https://example.com',
-        'field': 'key',
+        'field': 'key1',
+    }
+    backend = JsonHttp(**config)
+    value = backend.get()
+    assert len(responses.calls) == 1
+    assert value == {'key1': 'value1'}
+
+
+@responses.activate
+def test_heroku_backend_no_field():
+    response = {
+        "key1": "value1",
+        "key2": "value2",
+    }
+    responses.add(responses.GET, 'https://example.com',
+        json=response)
+
+    config = {
+        'url': 'https://example.com',
     }
     backend = JsonHttp(**config)
     value = backend.get()
