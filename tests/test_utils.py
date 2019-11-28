d7335b7e598773531c30bea6830e4624a1d70f07
Tarjei Husøy <git@thusoy.com>
2019-11-28 16:51:46 -0300

    Refactor utils test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,20 +1,13 @@
 import pytest
 
-from cachish import utils as uut
+from cachish import utils
 
-_MERGE_DICT_TESTS = (
+
+@pytest.mark.parametrize('input1,input2,expected', [
     ({'a': 1}, {'b': 2}, {'a': 1, 'b': 2}),
     ({'a': 1}, {'a': 2}, {'a': 2}),
     ({'a': {'b': 1}}, {'a': {'c': 2}}, {'a': {'b': 1, 'c': 2}}),
-)
-
-
-@pytest.fixture(params=_MERGE_DICT_TESTS)
-def merge_dicts(request):
-    yield request.param
-
-
-def test_merge_dicts(merge_dicts):
-    a, b = merge_dicts[0], merge_dicts[1]
-    uut.merge_dicts(a, b)
-    assert a == merge_dicts[2]
+])
+def test_merge_dicts(input1, input2, expected):
+    utils.merge_dicts(input1, input2)
+    assert input1 == expected
